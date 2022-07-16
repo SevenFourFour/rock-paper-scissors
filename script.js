b6493c51c0536a28c1cfd44f4393cab8d6443d6f
Julian <code@sevenfourfour.net>
2022-07-16 18:32:13 -0500

    Add buttons for gui in index.html

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
 var choices = ['Rock', 'Paper', 'Scissors']
 
-function computerPlay(){
+function computerPlay() {
     var index = Math.floor(Math.random() * 3)
     return index
 }
 
-function playerPlay(){
+function playerPlay() {
     var choice = prompt('Make your choice:\n[1] for Rock\n[2] for Paper\n[3] for Scissors')
     return choice - 1
 }
@@ -21,12 +21,12 @@ function playerPlay(){
 // Example: "You lose! Paper beats Rock" 
 // PS: Make playerSelection parameter case-insensitive
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     console.log(choices[playerSelection])
     console.log(choices[computerSelection])
-    switch(playerSelection){
+    switch (playerSelection) {
         case 0:
-            switch(computerSelection){
+            switch (computerSelection) {
                 case 0:
                     return 'You tied this round!'
                     break;
@@ -38,7 +38,7 @@ function playRound(playerSelection, computerSelection){
             }
             break;
         case 1:
-            switch(computerSelection){
+            switch (computerSelection) {
                 case 0:
                     return choices[playerSelection] + ' wins to ' + choices[computerSelection] + '. You win this round.'
                     break;
@@ -50,25 +50,25 @@ function playRound(playerSelection, computerSelection){
             }
             break;
         default:
-            case 0:
-                switch(computerSelection){
-                    case 0:
-                        return choices[playerSelection] + ' loses to ' + choices[computerSelection] + '. You lose this round.'
-                        break;
-                    case 1:
-                        return choices[playerSelection] + ' wins to ' + choices[computerSelection] + '. You win this round.'
-                        break;
-                    default:
-                        return 'You tied this round!'
-                }
-                break;
+        case 0:
+            switch (computerSelection) {
+                case 0:
+                    return choices[playerSelection] + ' loses to ' + choices[computerSelection] + '. You lose this round.'
+                    break;
+                case 1:
+                    return choices[playerSelection] + ' wins to ' + choices[computerSelection] + '. You win this round.'
+                    break;
+                default:
+                    return 'You tied this round!'
+            }
+            break;
     }
 }
 
 
 // Write a function called game() that calls playRound to
 // to play a 5 round game and reports a winner at the end
-
+/*
 function game(){
     var playerScore = 0
     var computerScore = 0
@@ -98,6 +98,10 @@ function game(){
     }
 
 }
+*/
+
+
+
 
 
 console.log(game())
